7d48625513ef4b6a5087a55b2656e6aa65843d70
Saha <chiranjit.saha@optum.com>
2020-02-09 22:19:23 +0530

    Updates

---
+++ precache-manifest.16b9fe3466ef71ca99013508b49597de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589312efccb450bbd622f5848a8a7fa4",
+    "url": "/RoboFriend/index.html"
+  },
+  {
+    "revision": "415a57bfd0cd3e789b84",
+    "url": "/RoboFriend/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "11c4f1460a66d5084942",
+    "url": "/RoboFriend/static/css/main.f0b98bc7.chunk.css"
+  },
+  {
+    "revision": "415a57bfd0cd3e789b84",
+    "url": "/RoboFriend/static/js/2.08876ecc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/RoboFriend/static/js/2.08876ecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11c4f1460a66d5084942",
+    "url": "/RoboFriend/static/js/main.a839d702.chunk.js"
+  },
+  {
+    "revision": "112ca840b3d92a5aa393",
+    "url": "/RoboFriend/static/js/runtime-main.27db9880.js"
+  }
+]);
